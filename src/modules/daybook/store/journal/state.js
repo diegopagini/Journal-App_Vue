bb5970cb120acb09f2bc974e12345509b20f484b
Diego Pagini <diego.pagini@neoris.com>
2023-08-07 12:10:05 +0200

    entries

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -2,5 +2,36 @@
 
 export default () => ({
 	isLoading: false,
-	entries: [],
+	entries: [
+		{
+			id: new Date().getTime(),
+			date: new Date().toDateString(),
+			text: 'Cillum sunt minim deserunt eu cupidatat deserunt fugiat culpa tempor sunt laborum reprehenderit aliqua sint. Do in ea reprehenderit do. Excepteur anim aliqua sunt deserunt et excepteur voluptate cupidatat labore labore excepteur. Minim commodo deserunt irure sunt consectetur irure fugiat tempor labore culpa ut sunt et. Enim non in velit quis culpa deserunt esse nostrud incididunt et qui consequat. Reprehenderit esse cillum eiusmod sint consequat non eu.',
+			picture: null,
+		},
+		{
+			id: new Date().getTime() + 100,
+			date: new Date().toDateString(),
+			text: 'Cillum sunt minim deserunt eu cupidatat deserunt fugiat culpa tempor sunt laborum reprehenderit aliqua sint. Do in ea reprehenderit do. Excepteur anim aliqua sunt deserunt et excepteur voluptate cupidatat labore labore excepteur. Minim commodo deserunt irure sunt consectetur irure fugiat tempor labore culpa ut sunt et. Enim non in velit quis culpa deserunt esse nostrud incididunt et qui consequat. Reprehenderit esse cillum eiusmod sint consequat non eu.',
+			picture: null,
+		},
+		{
+			id: new Date().getTime() + 200,
+			date: new Date().toDateString(),
+			text: 'Cillum sunt minim deserunt eu cupidatat deserunt fugiat culpa tempor sunt laborum reprehenderit aliqua sint. Do in ea reprehenderit do. Excepteur anim aliqua sunt deserunt et excepteur voluptate cupidatat labore labore excepteur. Minim commodo deserunt irure sunt consectetur irure fugiat tempor labore culpa ut sunt et. Enim non in velit quis culpa deserunt esse nostrud incididunt et qui consequat. Reprehenderit esse cillum eiusmod sint consequat non eu.',
+			picture: null,
+		},
+		{
+			id: new Date().getTime() + 300,
+			date: new Date().toDateString(),
+			text: 'Cillum sunt minim deserunt eu cupidatat deserunt fugiat culpa tempor sunt laborum reprehenderit aliqua sint. Do in ea reprehenderit do. Excepteur anim aliqua sunt deserunt et excepteur voluptate cupidatat labore labore excepteur. Minim commodo deserunt irure sunt consectetur irure fugiat tempor labore culpa ut sunt et. Enim non in velit quis culpa deserunt esse nostrud incididunt et qui consequat. Reprehenderit esse cillum eiusmod sint consequat non eu.',
+			picture: null,
+		},
+		{
+			id: new Date().getTime() + 400,
+			date: new Date().toDateString(),
+			text: 'Cillum sunt minim deserunt eu cupidatat deserunt fugiat culpa tempor sunt laborum reprehenderit aliqua sint. Do in ea reprehenderit do. Excepteur anim aliqua sunt deserunt et excepteur voluptate cupidatat labore labore excepteur. Minim commodo deserunt irure sunt consectetur irure fugiat tempor labore culpa ut sunt et. Enim non in velit quis culpa deserunt esse nostrud incididunt et qui consequat. Reprehenderit esse cillum eiusmod sint consequat non eu.',
+			picture: null,
+		},
+	],
 });
