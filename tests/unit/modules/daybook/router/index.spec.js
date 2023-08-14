7d9ab74279f0a2cb470c5ca28650174cf2803d77
Diego Pagini <diego.pagini@neoris.com>
2023-08-14 14:26:58 +0200

    test in router

--- tests/unit/modules/daybook/router/index.spec.js
+++ tests/unit/modules/daybook/router/index.spec.js
@@ -16,14 +16,32 @@ describe('Daybook router', () => {
 				},
 			],
 		});
+	});
 
+	it('Should contain routes', async () => {
 		expect((await daybookRouter.children[0].component()).default.name).toEqual('no-entry');
 		expect((await daybookRouter.children[1].component()).default.name).toBe('entry-view');
+	});
 
+	it('Should contain routes', async () => {
 		const promiseRoutes = [];
 		daybookRouter.children.forEach((child) => promiseRoutes.push(child.component()));
 
 		const routes = (await Promise.all(promiseRoutes)).map((route) => route.default.name);
 		expect(routes).toContain('no-entry', 'entry-view');
+	});
+
+	it('Should return the id of the route', () => {
+		const route = {
+			params: {
+				id: 'ABC-123',
+			},
+		};
+
+		expect(daybookRouter.children[1].props(route)).toEqual({ id: 'ABC-123' });
+
+		// Better
+		const entryRoute = daybookRouter.children.find((r) => r.name === 'entry');
+		expect(entryRoute.props(route)).toEqual({ id: 'ABC-123' });
 	});
 });
