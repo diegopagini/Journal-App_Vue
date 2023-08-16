c8c8300ca64655b6bc64713869e3e94606325906
Diego Pagini <diego.pagini@neoris.com>
2023-08-16 13:22:19 +0200

    store

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -1,9 +1,10 @@
 /** @format */
-
 import axios from 'axios';
 
 const journalApi = axios.create({
 	baseURL: 'https://vue-demo-835ba-default-rtdb.europe-west1.firebasedatabase.app',
 });
+
+console.log(process.env.NODE_ENV);
 
 export default journalApi;
--- tests/unit/modules/daybook/store/journal/journal-module.spec.js
+++ tests/unit/modules/daybook/store/journal/journal-module.spec.js
@@ -93,8 +93,48 @@ describe('Vuex - Journal Module', () => {
 		});
 
 		store.commit('journal/deleteEntry', '-NbnHbDAWZvXdrl1ZZei');
-		expect(store.state.journal.entries[0].length).toEqual(0);
+		expect(store.state.journal.entries.length).toEqual(0);
 	});
 
 	// Getters
+	it('getters: getEntriesByTerm getEntryById', () => {
+		const store = createVuexStore(journalState);
+
+		expect(store.getters['journal/getEntriesByTerm']('').length).toBe(4);
+		expect(store.getters['journal/getEntriesByTerm']('bonito').length).toBe(1);
+
+		// By Id
+		const [entry1] = journalState.entries;
+		expect(store.getters['journal/getEntryById']('-NbnHbDAWZvXdrl1ZZei')).toEqual(entry1);
+	});
+
+	// Actions
+	it('actions: loadEntries', async () => {
+		const store = createVuexStore({
+			isLoading: true,
+			entries: [],
+		});
+
+		await store.dispatch('journal/loadEntries');
+		expect(store.state.journal.entries.length).toBeGreaterThan(0);
+	});
+
+	it('actions: updateEntry', async () => {
+		const store = createVuexStore(journalState);
+		const entry = { ...journalState.entries[0], text: 'this is a test' };
+		await store.dispatch('journal/updateEntry', entry);
+		expect(store.state.journal.entries[0]).toEqual(entry);
+	});
+
+	it('actions: createEntry deleteEntry', async () => {
+		const store = createVuexStore(journalState);
+		const newEntry = { date: 1627077227978, text: 'Nueva entrada de prueba' };
+		const id = await store.dispatch('journal/createEntry', newEntry);
+		expect(typeof id).toBe('string');
+
+		expect(store.state.journal.entries.find((el) => el.id === id)).toBeTruthy();
+
+		await store.dispatch('journal/deleteEntry', id);
+		expect(store.state.journal.entries.find((el) => el.id === id)).toBeFalsy();
+	});
 });
